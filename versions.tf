948b5660e62b0687f42eb19f57d630508e717212
BobbyCoppersmithEnv0 <robert.coppersmith@env0.com>
2026-04-25 14:33:30 -0700

    Add Terraform version and provider requirements

--- versions.tf
+++ versions.tf
@@ -1,1 +1,11 @@
 # Terraform version requirements
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    env0 = {
+      source  = "env0/env0"
+      version = "~> 1.29"
+    }
+  }
+}
